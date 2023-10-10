7775e53de728f4054749c34cb0f47fc636ac2a2f
imabd <imabdullahajmal@gmail.com>
2023-10-10 23:15:04 +0500

    Added tasks 1 and 2 from lab 1

---
+++ Lab1Task1.cpp
@@ -0,0 +1,23 @@
+//NAME : Abdullah Ajmal
+//SAP : 47729
+//TASK : 1
+
+#include <iostream>
+using namespace std;
+int main(){
+	
+    const int sizeOfArray = 10;
+    int arr[sizeOfArray] = {1};
+    int *ptr = arr;
+    
+    
+    for(int i = 0; i<sizeOfArray ; i+=1){
+    	cout<<">>";cin>>*(ptr+(4*i));
+	}
+	
+	for(int i = 0; i<sizeOfArray ; i+=1){
+    	cout<<"Element at the index "<<i<<" is "<<*(ptr+(4*i))<<endl;
+	}
+	
+	return 0;
+}
